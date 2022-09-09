4bb348436351a45bbcca9efa59939155b0594c95
Edward Beh <edward_beh_from.scientec@stb.gov.sg>
2022-09-10 00:34:05 +0800

    center HeroSection of profile in mobile

--- src/pages/Dashboard/Home/components/HeroSection/HeroSection.tsx
+++ src/pages/Dashboard/Home/components/HeroSection/HeroSection.tsx
@@ -6,18 +6,18 @@ const HeroSection = () => {
   return (
     <div className="w-full">
       <Card>
-        <div className="flex p-2 space-x-6">
+        <div className="flex items-center sm:items-start flex-col sm:flex-row p-2 space-x-0 sm:space-x-6">
           <img
             src={imgJohnDoe}
             alt="profile"
             className="object-cover min-h-[100px] min-w-[100px] max-h-[100px] max-w-[100px] border-2 border-white rounded-full ring-cyan-500 ring-2"
           />
 
-          <div>
+          <div className="text-center sm:text-left mt-3 sm:mt-0">
             <h1 className="text-2xl font-semibold">Dr. John Doe</h1>
             <p className="pt-1">Senior Consultant</p>
 
-            <p className="pt-5 text-base">
+            <p className="pt-2 sm:pt-4 text-base">
               Renal Department, Sample General Hospital
             </p>
           </div>
