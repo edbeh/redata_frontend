53e483d1d80c002f3d99000bc90769b75d5f378f
edward <edbeh89@gmail.com>
2022-10-30 18:32:10 +0800

    update toast styling

--- src/wrapper-components/AxiosErrorInterceptor/AxiosErrorInterceptor.tsx
+++ src/wrapper-components/AxiosErrorInterceptor/AxiosErrorInterceptor.tsx
@@ -1,5 +1,7 @@
+// @ts-nocheck
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./AxiosErrorInterceptor.css";
 
 interface AxiosErrorInterceptorProps {
   children: React.ReactNode;
@@ -8,9 +10,25 @@ interface AxiosErrorInterceptorProps {
 const AxiosErrorInterceptor: React.FC<AxiosErrorInterceptorProps> = ({
   children,
 }) => {
+  const contextClass = {
+    success: "bg-white border-l-green-500",
+    error: "bg-white border-l-red-500",
+    info: "bg-white",
+    warning: "bg-white border-l-yellow-500",
+    default: "bg-white",
+  };
+
   return (
     <>
-      <ToastContainer theme="colored" position="bottom-right" />
+      <ToastContainer
+        position="bottom-center"
+        toastClassName={({ type }) =>
+          contextClass[type || "default"] +
+          " relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer min-w-[350px] shadow-lg border-[1px] border-bordergray border-l-[6px]"
+        }
+        bodyClassName={() => "flex text-black block p-3"}
+        progressClassName="ml-[2px]"
+      />
       {children}
     </>
   );
