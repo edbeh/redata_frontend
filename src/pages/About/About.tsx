60571ecf548c251ac9e5ff9203c9fc452bbb5ea4
edward <edbeh89@gmail.com>
2022-11-17 16:11:30 +0800

    add footer for landing apge

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -3,36 +3,41 @@ import { getFeatureContent } from "./About.util";
 import TopNav from "./components/TopNav/TopNav";
 import Hero from "./components/Hero/Hero";
 import Feature from "./components/Feature/Feature";
+import Footer from "./components/Footer/Footer";
 
 const About = () => {
   const featureContent = getFeatureContent();
 
   return (
-    <div className="px-6 pb-[100px] lg:pb-[200px]">
-      <TopNav />
-      <Hero />
+    <div>
+      <div className="px-6 pb-[30px]">
+        <TopNav />
+        <Hero />
 
-      {featureContent.map((feature) => {
-        const { number, tagline, title, gradientStart, gradientEnd, content } =
-          feature;
-        return (
-          <Feature
-            key={title}
-            number={number}
-            tagline={tagline}
-            title={title}
-            gradientStart={gradientStart}
-            gradientEnd={gradientEnd}
-            content={content}
-          />
-        );
-      })}
+        {featureContent.map((feature) => {
+          const {
+            number,
+            tagline,
+            title,
+            gradientStart,
+            gradientEnd,
+            content,
+          } = feature;
+          return (
+            <Feature
+              key={title}
+              number={number}
+              tagline={tagline}
+              title={title}
+              gradientStart={gradientStart}
+              gradientEnd={gradientEnd}
+              content={content}
+            />
+          );
+        })}
+      </div>
 
-      {/* <div className="flex justify-center">
-        <p className="mt-[80px] lg:mt-[120px] text-sm font-bold text-gray-500 tracking-wider">
-          LET'S GET STARTED
-        </p>
-      </div> */}
+      <Footer />
     </div>
   );
 };
---
+++ src/pages/About/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black">
+      <div className="mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <p className="text-center text-white">
+          &copy; 2022 ReData. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
