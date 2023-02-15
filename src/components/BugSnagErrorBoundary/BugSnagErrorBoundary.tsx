1998e6211e35599ebfe5f8034346a272d57133c2
edward <edbeh89@gmail.com>
2023-02-15 10:57:32 +0800

    hide bugsnag api key, update csp to allow bugsnag session tracking

--- src/components/BugSnagErrorBoundary/BugSnagErrorBoundary.tsx
+++ src/components/BugSnagErrorBoundary/BugSnagErrorBoundary.tsx
@@ -3,12 +3,16 @@ import Bugsnag from "@bugsnag/js";
 import BugsnagPluginReact, {
   BugsnagPluginReactResult,
 } from "@bugsnag/plugin-react";
+import { browser } from "@bugsnag/source-maps";
 
+// start service
 Bugsnag.start({
   apiKey: "5984a70a690788253f53643d5b481dab",
   appVersion: "1.0.0",
   plugins: [new BugsnagPluginReact()],
 });
+
+console.log("bugsnag started");
 
 const plugin = Bugsnag.getPlugin("react") as BugsnagPluginReactResult;
 const ErrorBoundary = plugin?.createErrorBoundary(React);
