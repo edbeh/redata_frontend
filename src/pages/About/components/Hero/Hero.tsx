91492b6d5a99eed9c048d51d72189b9ea954d663
edward <edbeh89@gmail.com>
2023-02-18 09:52:13 +0800

    remove bugsnag notify for testing

--- src/pages/About/components/Hero/Hero.tsx
+++ src/pages/About/components/Hero/Hero.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Bugsnag from "@bugsnag/js";
 
 import { Button } from "components";
 import { navigateToSubdomain } from "utils";
@@ -83,12 +82,7 @@ const Hero = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Button
-            variant="secondary"
-            onClick={() => Bugsnag.notify(new Error("Test bugsnag error"))}
-          >
-            View Sample Profile
-          </Button>
+          <Button variant="secondary">View Sample Profile</Button>
         </a>
       </div>
 
