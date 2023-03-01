e43d3d2dbb7d70d759161e09cc573da2e0a32790
edward <edbeh89@gmail.com>
2023-03-01 17:08:57 +0800

    throw custom error to test bugsnag integration

--- src/pages/About/components/Footer/Footer.tsx
+++ src/pages/About/components/Footer/Footer.tsx
@@ -2,7 +2,12 @@ const Footer = () => {
   return (
     <footer className="bg-black">
       <div className="mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <p className="text-center text-white">
+        <p
+          className="text-center text-white"
+          onClick={() => {
+            throw new Error("test bugsnag sourcemap integration");
+          }}
+        >
           &copy; 2022 ReData. All rights reserved.
         </p>
       </div>
