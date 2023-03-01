912969479b239530aa29487aa0067f0ea27ec3f9
edward <edbeh89@gmail.com>
2023-03-01 17:16:04 +0800

    remove custom error

--- src/pages/About/components/Footer/Footer.tsx
+++ src/pages/About/components/Footer/Footer.tsx
@@ -2,12 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-black">
       <div className="mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <p
-          className="text-center text-white"
-          onClick={() => {
-            throw new Error("test bugsnag sourcemap integration");
-          }}
-        >
+        <p className="text-center text-white">
           &copy; 2022 ReData. All rights reserved.
         </p>
       </div>
