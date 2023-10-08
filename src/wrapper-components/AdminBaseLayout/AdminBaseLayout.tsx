943d4972044e326789379788a99c0f4f20817a09
edward <edbeh89@gmail.com>
2023-10-08 15:10:22 +0800

    adjust z index

--- src/wrapper-components/AdminBaseLayout/AdminBaseLayout.tsx
+++ src/wrapper-components/AdminBaseLayout/AdminBaseLayout.tsx
@@ -42,7 +42,7 @@ const AdminBaseLayout: React.FC<AdminBaseLayoutProps> = ({
       </div>
 
       <div className="flex w-full flex-col lg:ml-[250px] bg-backgroundGray">
-        <div className="sticky top-0 flex w-full z-30 justify-between border-b-[1px] border-b-bordergray bg-white py-5 pl-8 pr-4 font-bold lg:px-8">
+        <div className="sticky top-0 flex w-full justify-between border-b-[1px] border-b-bordergray bg-white py-5 pl-8 pr-4 font-bold lg:px-8">
           <div
             className={`flex items-center ${
               withBackNavigation ? "cursor-pointer" : ""
