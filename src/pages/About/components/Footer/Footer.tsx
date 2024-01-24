af4fa46ebd7d0378131e237b620d96bd2515976f
Edward <edbeh89@gmail.com>
2024-01-24 20:57:55 +0800

    dynamic year at landing page footer

--- src/pages/About/components/Footer/Footer.tsx
+++ src/pages/About/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-black">
       <div className="mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <p className="text-center text-white">
-          &copy; 2022 ReData. All rights reserved.
+          &copy; {new Date().getFullYear()} ReData. All rights reserved.
         </p>
       </div>
     </footer>
